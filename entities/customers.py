5cd1d04af6a6b1748abadc22505a6cd3e52d99a9
Alejandro F <afuste7@gmail.com>
2021-11-22 16:04:30 -0500

    added customer class to customers.py file

---
+++ entities/customers.py
@@ -0,0 +1,21 @@
+# from typing import Dict
+
+
+class Customer:
+    # def __init__(self, first_name: str, last_name: str, user_name: str, customer_id: int, accounts: Dict[str, Dict]):
+    def __init__(self, first_name: str, last_name: str, user_name: str, customer_id: int):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.customer_id = customer_id
+        # self.accounts = accounts
+
+    def make_customer_dictionary(self):
+        return {
+            "firstName": self.first_name,
+            "lastName": self.last_name,
+            "userName": self.user_name,
+            "customerId": self.customer_id,
+            # "self.accounts": self.accounts
+        }
+
