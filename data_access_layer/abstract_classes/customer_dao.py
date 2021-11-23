6749761447b5c885024f2ee0813615b273b35be8
Alejandro F <afuste7@gmail.com>
2021-11-23 14:15:44 -0500

    added the rest of the abstract methods

--- data_access_layer/abstract_classes/customer_dao.py
+++ data_access_layer/abstract_classes/customer_dao.py
@@ -25,3 +25,20 @@ class CustomerDao(ABC):
     @abstractmethod
     def transfer_money_between_accounts_by_their_ids(self, from_account_id: int, to_account_id: int) -> Customer:
         pass
+
+    @abstractmethod
+    def update_customer_by_id(self, customer_id: int) -> Customer:
+        pass
+
+    @abstractmethod
+    def get_customer_by_id(self, customer_id: int) -> Customer:
+        pass
+
+    @abstractmethod
+    def delete_account_by_id(self, customer_id: int) -> bool:
+        pass
+
+    @abstractmethod
+    def delete_customer_by_id(self, customer_id: int) -> bool:
+        pass
+    
