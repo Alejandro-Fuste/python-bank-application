fb534c46f0a2d5fd6d668bff97f2bb8a6dacfad4
Alejandro F <afuste7@gmail.com>
2021-11-23 11:06:40 -0500

    removed folders and added new packages

--- entities/customers.py
+++ entities/customers.py
@@ -1,14 +1,13 @@
-# from typing import Dict
+from typing import Dict
 
 
 class Customer:
-    # def __init__(self, first_name: str, last_name: str, user_name: str, customer_id: int, accounts: Dict[str, Dict]):
-    def __init__(self, first_name: str, last_name: str, user_name: str, customer_id: int):
+    def __init__(self, first_name: str, last_name: str, user_name: str, customer_id: int, accounts: Dict[str, Dict]):
         self.first_name = first_name
         self.last_name = last_name
         self.user_name = user_name
         self.customer_id = customer_id
-        # self.accounts = accounts
+        self.accounts = accounts
 
     def make_customer_dictionary(self):
         return {
@@ -16,6 +15,10 @@ class Customer:
             "lastName": self.last_name,
             "userName": self.user_name,
             "customerId": self.customer_id,
-            # "self.accounts": self.accounts
+            "self.accounts": self.accounts
         }
 
+
+customer_1 = Customer("Luke", "Skywalker", "Master Jedi", 1, {"checking": {}})
+print(customer_1)
+print(customer_1.make_customer_dictionary())
