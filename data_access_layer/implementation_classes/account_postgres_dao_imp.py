9045c8d5e0cce8a19d11a1583b37ec2eacbb97ab
Alejandro F <afuste7@gmail.com>
2021-12-01 17:37:54 -0500

    added imports to customer progress_dao_imp

---
+++ data_access_layer/implementation_classes/account_postgres_dao_imp.py
@@ -0,0 +1,8 @@
+from typing import List
+
+from data_access_layer.abstract_classes.account_dao import AccountDao
+from entities.accounts import Account
+from util.database_connection import connection
+
+
+
