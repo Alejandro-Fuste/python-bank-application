27da6955f3f9fe3834a8647b8e22a5ff1bf6f5f4
Alejandro F <afuste7@gmail.com>
2021-12-01 17:45:25 -0500

    imported abstract methods from accountDAO

--- data_access_layer/implementation_classes/account_postgres_dao_imp.py
+++ data_access_layer/implementation_classes/account_postgres_dao_imp.py
@@ -5,4 +5,16 @@ from entities.accounts import Account
 from util.database_connection import connection
 
 
+class AccountPostgresDAO(AccountDao):
 
+    def create_account(self, customer_id: int, account: Account) -> Account:
+        pass
+
+    def get_all_customers(self) -> List[Account]:
+        pass
+
+    def get_all_accounts(self) -> List[Account]:
+        pass
+
+    def get_all_customer_accounts_by_id(self, customer_id: int) -> List[Account]:
+        pass
