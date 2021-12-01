7d64ffdb0fc85135b6b53cad52d890a9d11131ad
Alejandro F <afuste7@gmail.com>
2021-12-01 17:41:19 -0500

    added back code into connection file

--- util/database_connection.py
+++ util/database_connection.py
@@ -1,19 +1,19 @@
-from psycopg2 import connect
+from psycopg2 import connect, OperationalError
 import os
 
 
 def create_connection():
-    # try:
-    conn = connect(
-        host=os.environ.get("HOST"),
-        dbname=os.environ.get("DB"),
-        user=os.environ.get("USER"),
-        password=os.environ.get("PASSWORD"),
-        port=os.environ.get("PORT")
-    )
-    return conn
-    # except OperationalError as e:
-    #     print(str(e))
+    try:
+        conn = connect(
+            host=os.environ.get("HOST"),
+            dbname=os.environ.get("DB"),
+            user=os.environ.get("USER"),
+            password=os.environ.get("PASSWORD"),
+            port=os.environ.get("PORT")
+        )
+        return conn
+    except OperationalError as e:
+        print(str(e))
 
 
 connection = create_connection()
