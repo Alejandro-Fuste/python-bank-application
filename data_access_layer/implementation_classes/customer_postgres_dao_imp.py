13e90221a6349ff6be470e69fe02b755e272aa9c
Alejandro F <afuste7@gmail.com>
2021-12-02 15:33:50 -0500

    added code for customer postgres dao imp create, update, get customer by id methods

--- data_access_layer/implementation_classes/customer_postgres_dao_imp.py
+++ data_access_layer/implementation_classes/customer_postgres_dao_imp.py
@@ -5,7 +5,13 @@ from entities.customers import Customer
 
 class CustomerPostgresDAO(CustomerDao):
     def create_customer_entry(self, customer: Customer) -> Customer:
-        pass
+        sql = "insert into customer values(default, %s, %s, %s) returning customer_id"
+        cursor = connection.cursor()
+        cursor.execute(sql, (customer.first_name, customer.last_name, customer.customer_id))
+        connection.commit()
+        customer_id = cursor.fetchone()[0]
+        customer.customer_id = customer_id
+        return customer
 
     def get_customer_balance_by_id(self, customer_id: int, account_id: int) -> float:
         pass
@@ -21,10 +27,19 @@ class CustomerPostgresDAO(CustomerDao):
         pass
 
     def update_customer_by_id(self, customer_id: int, customer: Customer) -> Customer:
-        pass
+        sql = 'update customer set first_name = %s, last_name = %s, user_name = %s where customer_id = %s'
+        cursor = connection.cursor()
+        cursor.execute(sql, (customer.first_name, customer.last_name, customer.user_name, customer.customer_id))
+        connection.commit()
+        return customer
 
     def get_customer_by_id(self, customer_id: int) -> Customer:
-        pass
+        sql = 'select * from customer where customer_id = %s'
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        customer_record = cursor.fetchone()
+        customer = Customer(*customer_record)
+        return customer
 
     def delete_account_by_id(self, customer_id: int, account_id: int) -> bool:
         pass
