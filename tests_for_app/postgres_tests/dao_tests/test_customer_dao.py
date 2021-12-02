8ed0ab78fd8abf8be86910e1b2d25fcb50b00fcd
Alejandro F <afuste7@gmail.com>
2021-12-01 23:05:25 -0500

    added code for update and get customer tests

--- tests_for_app/postgres_tests/dao_tests/test_customer_dao.py
+++ tests_for_app/postgres_tests/dao_tests/test_customer_dao.py
@@ -32,15 +32,19 @@ def test_withdraw_from_account_by_id_success():
 
 
 def test_transfer_money_by_their_ids_success():
-    pass
+    to_account_original_balance = customer_dao.get_customer_balance_by_id(1, 1)
+    to_account_new_balance = customer_dao.transfer_money_by_their_ids(1, 2, 1, 100)
+    assert to_account_new_balance > to_account_original_balance
 
 
 def test_update_customer_by_id_success():
-    pass
+    customer_update: Customer = customer_dao.update_customer_by_id(1, update_customer)
+    assert customer_update.user_name == update_customer.user_name
 
 
 def test_get_customer_by_id_success():
-    pass
+    selected_customer = customer_dao.get_customer_by_id(1)
+    assert selected_customer.customer_id == 1
 
 
 def test_delete_account_by_id_success():
