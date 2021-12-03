93050848f373eb7d740f0512dabe2b17e229274c
Alejandro F <afuste7@gmail.com>
2021-12-02 21:36:25 -0500

    added service in front of all method names

--- service_layer/abstract_services/account_service.py
+++ service_layer/abstract_services/account_service.py
@@ -6,17 +6,17 @@ from entities.accounts import Account
 class AccountService(ABC):
 
     @abstractmethod
-    def create_account(self, account: Account) -> Account:
+    def service_create_account(self, account: Account) -> Account:
         pass
 
     @abstractmethod
-    def get_all_accounts(self) -> List[Account]:
+    def service_get_all_accounts(self) -> List[Account]:
         pass
 
     @abstractmethod
-    def get_all_customer_accounts_by_id(self, customer_id: int) -> List[Account]:
+    def service_get_all_customer_accounts_by_id(self, customer_id: int) -> List[Account]:
         pass
 
     @abstractmethod
-    def delete_account_by_id(self, customer_id: str, account_id: int) -> bool:
+    def service_delete_account_by_id(self, customer_id: str, account_id: int) -> bool:
         pass
