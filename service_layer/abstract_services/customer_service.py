f77be2b9fe790d4b58c1601f6550966c091a761e
Alejandro F <afuste7@gmail.com>
2021-12-02 21:37:32 -0500

    added service in front of all method names

--- service_layer/abstract_services/customer_service.py
+++ service_layer/abstract_services/customer_service.py
@@ -6,38 +6,38 @@ from entities.customers import Customer
 class CustomerService(ABC):
 
     @abstractmethod
-    def create_customer_entry(self, customer: Customer) -> Customer:
+    def service_create_customer_entry(self, customer: Customer) -> Customer:
         pass
 
     @abstractmethod
-    def get_customer_by_id(self, customer_id: str) -> Customer:
+    def service_get_customer_by_id(self, customer_id: str) -> Customer:
         pass
 
     @abstractmethod
-    def get_all_customers(self) -> List[Customer]:
+    def service_get_all_customers(self) -> List[Customer]:
         pass
 
     @abstractmethod
-    def get_customer_balance_by_id(self, customer_id: str, account_id: int) -> int:
+    def service_get_customer_balance_by_id(self, customer_id: str, account_id: int) -> int:
         pass
 
     @abstractmethod
-    def deposit_into_account_by_id(self, customer_id: str, account_id: int, amount: int) -> int:
+    def service_deposit_into_account_by_id(self, customer_id: str, account_id: int, amount: int) -> int:
         pass
 
     @abstractmethod
-    def withdraw_from_account_by_id(self, customer_id: str, account_id: int, amount: int) -> int:
+    def service_withdraw_from_account_by_id(self, customer_id: str, account_id: int, amount: int) -> int:
         pass
 
     @abstractmethod
-    def transfer_money_by_their_ids(self, customer_id: str, from_account_id: int,
+    def service_transfer_money_by_their_ids(self, customer_id: str, from_account_id: int,
                                     to_account_id: int, amount: int) -> int:
         pass
 
     @abstractmethod
-    def update_customer_by_id(self, customer_id: str, customer: Customer) -> Customer:
+    def service_update_customer_by_id(self, customer_id: str, customer: Customer) -> Customer:
         pass
 
     @abstractmethod
-    def delete_customer_by_id(self, customer_id: str) -> bool:
+    def service_delete_customer_by_id(self, customer_id: str) -> bool:
         pass
