d02c01761e29e2416651cf0edf9be20ac3e038df
Alejandro F <afuste7@gmail.com>
2021-12-02 21:34:09 -0500

    added service in front of all method names

--- service_layer/implementation_services/account_postgres_service_imp.py
+++ service_layer/implementation_services/account_postgres_service_imp.py
@@ -1,11 +1,10 @@
-from typing import List
-
 from custom_exceptions.duplicate_customer_exception import DuplicateCustomerException
 from custom_exceptions.customer_not_found_exception import CustomerNotFoundException
 from custom_exceptions.invalid_transaction_exception import InvalidTransactionException
 from data_access_layer.implementation_classes.account_postgres_dao_imp import AccountPostgresDAO
 from entities.accounts import Account
 from service_layer.abstract_services.account_service import AccountService
+from typing import List
 
 
 class AccountPostgresServiceImp(AccountService):
@@ -15,11 +14,11 @@ class AccountPostgresServiceImp(AccountService):
     def create_account(self, account: Account) -> Account:
         pass
 
-    def get_all_customers(self) -> List[Account]:
-        pass
-
     def get_all_accounts(self) -> List[Account]:
         pass
 
     def get_all_customer_accounts_by_id(self, customer_id: int) -> List[Account]:
+        pass
+
+    def delete_account_by_id(self, customer_id: int, account_id: int) -> bool:
         pass
--- service_layer/implementation_services/customer_postgres_service_imp.py
+++ service_layer/implementation_services/customer_postgres_service_imp.py
@@ -4,36 +4,36 @@ from custom_exceptions.invalid_transaction_exception import InvalidTransactionEx
 from data_access_layer.implementation_classes.customer_postgres_dao_imp import CustomerPostgresDAO
 from entities.customers import Customer
 from service_layer.abstract_services.customer_service import CustomerService
-
+from typing import List
 
 class CustomerPostgresServiceImp(CustomerService):
     def __int__(self, customer_dao: CustomerPostgresDAO):
         self.customer_dao = customer_dao
 
-    def create_customer_entry(self, customer: Customer) -> Customer:
+    def service_create_customer_entry(self, customer: Customer) -> Customer:
         # customers = self
 
-    def get_customer_balance_by_id(self, customer_id: int, account_id: int) -> float:
+    def service_get_customer_by_id(self, customer_id: int) -> Customer:
+        pass
+
+    def service_get_all_customers(self) -> List[Customer]:
+        pass
+
+    def service_get_customer_balance_by_id(self, customer_id: int, account_id: int) -> float:
         # customers = self.customer_dao.
 
-    def deposit_into_account_by_id(self, customer_id: int, account_id: int, amount: int) -> float:
+    def service_deposit_into_account_by_id(self, customer_id: int, account_id: int, amount: int) -> float:
         pass
 
-    def withdraw_from_account_by_id(self, customer_id: int, account_id: int, amount: int) -> float:
+    def service_withdraw_from_account_by_id(self, customer_id: int, account_id: int, amount: int) -> float:
         pass
 
-    def transfer_money_by_their_ids(self, customer_id: int, from_account_id: int, to_account_id: float,
+    def service_transfer_money_by_their_ids(self, customer_id: int, from_account_id: int, to_account_id: float,
                                     amount: int) -> int:
         pass
 
-    def update_customer_by_id(self, customer_id: int, customer: Customer) -> Customer:
+    def service_update_customer_by_id(self, customer_id: int, customer: Customer) -> Customer:
         pass
 
-    def get_customer_by_id(self, customer_id: int) -> Customer:
-        pass
-
-    def delete_account_by_id(self, customer_id: int, account_id: int) -> bool:
-        pass
-
-    def delete_customer_by_id(self, customer_id: int) -> bool:
+    def service_delete_customer_by_id(self, customer_id: int) -> bool:
         pass
